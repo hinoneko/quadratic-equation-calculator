2fabbd145d5ef773e4deacaeb584b9f2a77ee12e
hinoneko <yanasiryk35@gmail.com>
2025-03-13 01:39:33 +0200

    implemented a general algorithm for calculating equations

---
+++ calculator.py
@@ -0,0 +1,24 @@
+import math
+
+
+def solve_quadratic(a, b, c):
+
+    if a == 0:
+        raise ValueError("Coefficient 'a' cannot be 0 (not a quadratic equation).")
+
+    discriminant = b ** 2 - 4 * a * c
+
+    print(f"Equation is: ({a}) x² + ({b}) x + ({c}) = 0")
+
+    if discriminant > 0:
+        x1 = (-b + math.sqrt(discriminant)) / (2 * a)
+        x2 = (-b - math.sqrt(discriminant)) / (2 * a)
+        print("There are 2 roots")
+        return [x1, x2]
+    elif discriminant == 0:
+        x = -b / (2 * a)
+        print("There are 1 roots")
+        return [x]
+    else:
+        print("There are 0 roots")
+        return []
